c2598763de56321b59303aecc0db6ff17e50143d
Julia Chen Jing <juliachenjing@gmail.com>
2017-03-14 23:53:10 -0500

    Add files via upload

---
+++ SimplestServletDemo/src/mum/cs472/SimplestServlet.java
@@ -0,0 +1,33 @@
+package mum.cs472;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Simplest")
+public class SimplestServlet extends HttpServlet {//建立一个类，扩展HttpServlet接口
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.print("<html><head><title>Test</title></head><body>");
+        out.print("<form method='post'>");
+        out.print("<p>from doGet method : Please click the button</p>");
+        out.print("<input type='submit' value='Click me'/>");
+        out.print("</form>");
+        out.print("</body></html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.print("<html><head><title>Test</title></head><body>");
+        out.print("<p>from doPost method: Postback received</p>");
+        out.print("</body></html>");
+    }
+}
