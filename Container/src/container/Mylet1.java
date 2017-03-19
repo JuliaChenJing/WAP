bbcecc28727545652cb640778a62f0328b07e86e
Julia Chen Jing <juliachenjing@gmail.com>
2017-03-19 15:30:29 -0500

    Add files via upload

---
+++ Container/src/container/Mylet1.java
@@ -0,0 +1,23 @@
+package container;
+import java.util.*;
+
+public class Mylet1 extends Mylet
+{
+    @Override
+    public void init()
+    {
+        System.out.println("In Mylet1.init");
+    }
+    
+    @Override
+    public void doGet(String method, Hashtable inputHeaders, Hashtable params, Hashtable outputHeaders, StringBuilder outputContent)
+    {
+        outputContent.append("<html>This is from doGet method of Mylet1</html>");
+    }
+
+    @Override
+    public void doPost(String method, Hashtable inputHeaders, Hashtable params, Hashtable outputHeaders, StringBuilder outputContent)
+    {        
+    }
+    
+}
