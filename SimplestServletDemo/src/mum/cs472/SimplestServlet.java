82e4c8bed9698f3ef312a9ab584901d7b10a5713
Julia Chen <juliachenjing@gmail.com>
2018-02-25 10:11:37 -0500

    updated

--- SimplestServletDemo/src/mum/cs472/SimplestServlet.java
+++ SimplestServletDemo/src/mum/cs472/SimplestServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/Simplest")
-public class SimplestServlet extends HttpServlet {//建立一个类，扩展HttpServlet接口
+public class SimplestServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
